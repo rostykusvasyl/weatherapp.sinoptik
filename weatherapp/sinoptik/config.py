42142b2b6cef4c24200869dc4bf99cc6edb9f910
rostykusvasyl <rostykusvasyl@gmail.com>
2019-02-01 21:30:09 +0200

    Basic configuration of the provider.

---
+++ weatherapp/sinoptik/config.py
@@ -0,0 +1,13 @@
+SINOPTIK_PROVIDER_NAME = 'sinoptik'
+SINOPTIK_PROVIDER_TITLE = 'sinoptik.ua'
+
+# The name of the city and the address for which we will display the weather
+# conditions by default for site sioptik.
+DEFAULT_SINOPTIK_LOCATION_NAME = 'Brody'
+DEFAULT_SINOPTIK_LOCATION_URL = ('https://ua.sinoptik.ua/%D0%BF%D'
+                                 '0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D'
+                                 '0%B1%D1%80%D0%BE%D0%B4%D0%B8')
+
+# Address for finding a place for which we will display weather conditions.
+SINOPTIK_BROWSE_LOCATIONS = ('https://ua.sinoptik.ua/%D1%83%D0%BA%D1%80%D'
+                             '0%B0%D1%97%D0%BD%D0%B0')
